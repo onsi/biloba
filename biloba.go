ee9a3158735edf6737977463a70fd0bba40e20c4
Onsi Fakhouri <onsijoe@gmail.com>
2023-03-09 20:53:31 -0700

    v0.1.0

--- biloba.go
+++ biloba.go
@@ -29,7 +29,7 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
-const BILOBA_VERSION = "0.0.0"
+const BILOBA_VERSION = "0.1.0"
 
 /*
 GinkgoTInterface is the interface by which Biloba receives GinkgoT()
